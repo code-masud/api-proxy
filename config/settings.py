7cdae2848ad80b58d817e281e402baffb4a8c0bc
code-masud <code.masud@hotmail.com>
2026-02-25 13:31:46 +0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,9 +25,9 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == True
+DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.getenv('SECRET_KEY', 'ALLOWED_HOSTS').split(",")
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(",")
 
 
 # Application definition
